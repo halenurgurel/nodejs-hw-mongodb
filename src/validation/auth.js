d74a3699511a308e722fe3ce593776f1224753a0
Halenur Gurel <halenur.gurel@hotmail.com>
2026-03-14 13:01:09 +0300

    fix: validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -4,7 +4,7 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().required(),
-  password: Joi.string().required,
+  password: Joi.string().required(),
 });
 
 //Login section
