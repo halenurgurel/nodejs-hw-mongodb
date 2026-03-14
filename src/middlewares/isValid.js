cec9d5ff776091dcb0a4187ead321419ce5742d8
Halenur Gurel <halenur.gurel@hotmail.com>
2026-03-14 12:32:40 +0300

    fix: isValid.js

--- src/middlewares/isValid.js
+++ src/middlewares/isValid.js
@@ -1,10 +1,10 @@
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
-//if studentId cannot be found, throw not found
+//if contactId cannot be found, throw not found
 export const isValidId = (req, res, next) => {
-  const { studentId } = req.params;
-  if (!isValidObjectId(studentId)) {
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
     throw createHttpError(404, 'Not Found');
   }
   next();
