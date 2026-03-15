6ac1f604aa1f476fa1e4d50eb89f5628fe9cb72a
Halenur Gurel <halenur.gurel@hotmail.com>
2026-03-15 13:21:38 +0300

    fix: register response body

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -22,10 +22,13 @@ const setupSession = (res, session) => {
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
 
+  const userData = user.toObject();
+  delete userData.password;
+
   res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
-    data: user,
+    data: userData,
   });
 };
 
